c1624d130286361185629dbf6845d15c6921ebe3
David Schmidt <schmdt628@gmail.com>
2015-06-02 14:42:25 -0400

    user edit, and partial list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
 	def show
 		@user = User.find(params[:id])
 
@@ -12,6 +13,7 @@ class UsersController < ApplicationController
   	def create
   		@user = User.new(user_params)
   		if @user.save
+        log_in @user
   			flash[:sucess] = "Welcome to the sample app!"
   			redirect_to @user
   		else 
@@ -19,10 +21,33 @@ class UsersController < ApplicationController
   		end
   	end
 
+    def index
+      @users = User.all
+    end
+
+    def edit 
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+      else
+        render 'edit'
+      end
+    end
+
   	private
   		def user_params
   			params.require(:user).permit(:name, :email, :password,
   				:password_confirmation)
   		end
+
+      def logged_in_user
+        unless logged_in?
+          flash[:danger] = "Please log in"
+          redirect_to login_url
+        end
+      end
 
 end
